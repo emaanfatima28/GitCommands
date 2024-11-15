e326f598a7b8945bef33eee8c2b3f9f211030a38
Emaan Fatima <emaanfatima0613@gmail.com>
2024-11-15 11:20:56 +0500

    Added Main

---
+++ src/gitcomm/Main.java
@@ -0,0 +1,12 @@
+package gitcomm;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Welcome to the application!");
+		greetUser();
+	}
+
+	public static void greetUser() {
+		System.out.println("Hello, User!");
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module GitCommands {
+}
