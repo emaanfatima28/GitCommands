0f0ad3412929b15b464b113c50c6ea329a90040b
nasif1731 <nasif1731@gmail.com>
2024-11-15 11:28:05 +0500

    Introduced Bug

--- src/gitcomm/Main.java
+++ src/gitcomm/Main.java
@@ -3,7 +3,7 @@ package gitcomm;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the application!");
-		greetUser();
+		// greetUser(); // Removed this line to introduce a bug
 	}
 
 	public static void greetUser() {
