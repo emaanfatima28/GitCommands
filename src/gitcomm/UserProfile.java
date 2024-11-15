103bc4714fd36440003f411cce8232d97453c461
nasif1731 <nasif1731@gmail.com>
2024-11-15 12:05:50 +0500

    added bug-fix

---
+++ src/gitcomm/UserProfile.java
@@ -0,0 +1,16 @@
+package gitcomm;
+
+public class UserProfile {
+	private String name;
+	private String email;
+
+	public UserProfile(String name, String email) {
+		this.name = name;
+		this.email = email;
+	}
+
+	public void displayProfile() {
+		System.out.println("Name: " + name);
+		System.out.println("Email: " + email);
+	}
+}
