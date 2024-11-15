103b55f5712ccf2a65462a5f636c0b104f71c6bd
Emaan Fatima <emaanfatima0613@gmail.com>
2024-11-15 11:36:26 +0500

    Added Login

---
+++ src/gitcomm/Login.java
@@ -0,0 +1,7 @@
+package gitcomm;
+
+public class Login {
+	public boolean authenticate(String username, String password) {
+		return username.equals("admin") && password.equals("password123");
+	}
+}
