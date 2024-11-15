ea93b5ad72f6b6fdb869b163c5c516871a97cd90
nasif1731 <nasif1731@gmail.com>
2024-11-15 12:18:56 +0500

    Main branch updated

--- src/gitcomm/Main.java
+++ src/gitcomm/Main.java
@@ -2,11 +2,6 @@ package gitcomm;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Welcome to the application!");
-		greetUser();
-	}
-
-	public static void greetUser() {
-		System.out.println("Hello, User!");
+		System.out.println("Main branch update.");
 	}
 }
