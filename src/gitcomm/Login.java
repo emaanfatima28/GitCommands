b2c70e0c3405bcb1f0e3f21da28b6fa77b58dcf8
Emaan Fatima <emaanfatima0613@gmail.com>
2024-11-15 11:41:32 +0500

    Experimenting

--- src/gitcomm/Login.java
+++ src/gitcomm/Login.java
@@ -2,6 +2,11 @@ package gitcomm;
 
 public class Login {
 	public boolean authenticate(String username, String password) {
+		if (username == null || password == null) {
+		System.out.println("Invalid input");
+		return false;
+		}
+		System.out.println("Authenticating user...");
 		return username.equals("admin") && password.equals("password123");
-	}
+		}
 }
