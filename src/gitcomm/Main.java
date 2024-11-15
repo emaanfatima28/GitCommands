8aadf168637f8a153389cfcca2aafb53980d313e
Emaan Fatima <emaanfatima0613@gmail.com>
2024-11-15 12:22:59 +0500

    Resolved Conflicts in Main

--- src/gitcomm/Main.java
+++ src/gitcomm/Main.java
@@ -1,22 +1,8 @@
 package gitcomm;
 
 public class Main {
-	Login login=new Login();
-	UserProfile up;
 	public static void main(String[] args) {
+		System.out.println("Feature A work in progress.");
 		System.out.println("Main branch update.");
 	}
-	public void authenticate() {
-		try {
-			login.authenticate("Nehal","123");
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	public void profile() {
-		up=new UserProfile("Nehal","123");
-		up.displayProfile();
-		}
-	 
 }
